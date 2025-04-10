9b982ab6109ebfdaa17548b5a0188d541512b5c5
cdiagal <cdiagalprog@gmail.com>
2025-04-10 21:39:19 +0100

    Mejoras del README #15

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 <div align="justify">
 
+<p align="center">
+<img src= images/LogoTaskYourself.png width=200 heigh=200 >
+</p>
+
 
 # App-proyect "TaskYourself"
 
-
-<img src=images/java-logo.png width=50px heigh=70px> <img src=images/javaFx-logo.png width=150px heigh=50px> <img src=images/logotipo-css.png width=90px heigh=50px> <img src=images/apache-Maven-logo.png width=100px heigh=50px> <img src=images/SceneBuilder-Logo.png width=80px heigh=50px>
 
 ## **Descrición**
 
 TaskYourself es una aplicación intuitiva y eficiente diseñada para ayudar a los usuarios a gestionar sus tareas diarias. Permite varias opciones al usuario como crear, modificar, organizar y hacer seguimiento de todas las tareas en una lista. Con una interfaz limpia y sencilla, se busca que no resulte aburrido interactuar con la App y al mismo tiempo, que el usuario no necesite demasiados pasos para cualquiera de las opciones que se ofrecen sobre las tareas. Por otra parte, para darle seguimiento a las tareas, TalkYourself tiene un sistema de notificaciones integrado para ayudar al usuario a cumplir con los propósitos, generando productividad en sus quehaceres.
 
 ---
+## **Tecnologías usadas para el desarrollo:**
+
+<img src=images/java-logo.png width=40px heigh=70px> <img src=images/javaFx-logo.png width=100px heigh=50px> <img src=images/logotipo-css.png width=80px heigh=50px> <img src=images/apache-Maven-logo.png width=80px heigh=50px> <img src=images/SceneBuilder-Logo.png width=60px heigh=50px> <img src=images/sqlite-Logo.jpeg width=60px heigh=50px> <img src=images/git-Logo.png width=60px heigh=50px> <img src=images/drawio-Logo.png width=60px heigh=50px>
+
+---
+
+## **Indicaciones de instalación**
 
--- src/main/java/es/cdiagal/taskyourself/backend/controller/login/LoginController.java
+++ src/main/java/es/cdiagal/taskyourself/backend/controller/login/LoginController.java
@@ -1,14 +1,15 @@
 package es.cdiagal.taskyourself.backend.controller.login;
 
-import javax.swing.event.HyperlinkEvent;
-
 import com.jfoenix.controls.JFXButton;
 
+import es.cdiagal.taskyourself.initApp.MainApplication;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * Clase que controla todas las funciones que contiene la ventana 'Login'.
@@ -52,6 +53,89 @@ public class LoginController {
     protected JFXButton homeButton;
 
 
+    /**
+     * Funcion que lleva hacia la pantalla de tareas.
+     */
     @FXML
+    protected void onClicLogin(){
+        try {
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("tareaList.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 450, 600);
+            stage.setTitle("Tareas");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     
+    /**
+     * Funcion que lleva hacia la pantalla de tareas.
+     */
+    @FXML
+    protected void onClicGoogle(){
+        try {
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("tareaList.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 450, 600);
+            stage.setTitle("Tareas");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Funcion que lleva hacia la pantalla de tareas.
+     */
+    @FXML
+    protected void onClicApple(){
+        try {
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("tareaList.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 450, 600);
+            stage.setTitle("Tareas");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Funcion que lleva hacia la pantalla de tareas.
+     */
+    @FXML
+    protected void onClicFacebook(){
+        try {
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("tareaList.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 450, 600);
+            stage.setTitle("Tareas");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+        /**
+     * Funcion que accede a la ventana de Inicio.
+     */
+    @FXML
+    protected void onClicHome(){
+        try {
+            Stage stage = (Stage) homeButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("initApp.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 450, 600);
+            stage.setTitle("Login");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
